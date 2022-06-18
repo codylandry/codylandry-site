b8ee40299e6a671c126b055bb7377a4405d5a915
Cody Landry <cody.landry@inmar.com>
2022-06-17 22:00:28 -0400

    removed aws amplify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import Amplify, { Auth } from 'aws-amplify'
-import awsmobile from './aws-exports'
 import { hooks } from 'vue-hooks'
 import './stylus/main.styl'
 
-Amplify.configure(awsmobile)
 Vue.config.productionTip = false
 
 Vue.use(hooks)
